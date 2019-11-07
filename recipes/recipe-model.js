d532c0fbe27e1c1621b17ff41c4a60f4adfc4199
Megan Ennis <Megan@Megans-iMac-9.local>
2019-11-07 15:16:02 +0000

    Endpoint & helper method to get shopping list for given recipe

--- recipes/recipe-model.js
+++ recipes/recipe-model.js
@@ -7,9 +7,26 @@
 const db = require("../data/dbConfig")
 
 module.exports = {
-    getRecipes
+    getRecipes,
+    getShoppingList
 }
 
 function getRecipes() {
     return db("recipes")
+}
+
+// select r.recipe_id, r.recipe_title, riq.quantity, ing.ingredient_title 
+// from recipes as r
+// join recipe_ingredient_quantities as riq
+// on r.recipe_id = riq.recipe_id
+// join ingredients as ing
+// on riq.ingredient_id = ing.ingredient_id
+// where r.recipe_id=1;
+
+function getShoppingList(id) {
+    return db("recipes as r")
+    .join("recipe_ingredient_quantities as riq", "r.recipe_id", "riq.recipe_id")
+    .join("ingredients as ing", "riq.ingredient_id", "ing.ingredient_id")
+    .select("r.recipe_id", "r.recipe_title", "riq.quantity", "ing.ingredient_title")
+    .where({"r.recipe_id": id})
 }
--- recipes/recipe-router.js
+++ recipes/recipe-router.js
@@ -1,15 +1,31 @@
-const express = require('express');
-const recipes = require('./recipe-model');
+const express = require("express");
+const recipes = require("./recipe-model");
 const router = express.Router();
 
-router.get('/', (req, res) => {
-    recipes.getRecipes()
+router.get("/", (req, res) => {
+  recipes
+    .getRecipes()
     .then(recipes => {
-        res.json(recipes)
+      res.json(recipes);
     })
     .catch(err => {
-        res.status(500).json({message: "Could not get recipes" + err.message})
+      res.status(500).json({ message: "Could not get recipes" + err.message });
+    });
+});
+
+router.get("/:id/shoppingList", (req, res) => {
+  recipes
+    .getShoppingList(req.params.id)
+    .then(shoppingList => {
+      res.json(shoppingList);
     })
-})
+    .catch(err => {
+      res
+        .status(500)
+        .json({
+          message: `Could not get shopping list for recipe with id of ${rewq.params.id} : ${err.message}`
+        });
+    });
+});
 
 module.exports = router;
