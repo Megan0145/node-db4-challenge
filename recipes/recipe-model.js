a3ee6d79f4e3ad6ce31da3aa1244c7b591565be4
Megan Ennis <Megan@Megans-iMac-9.local>
2019-11-07 13:27:54 +0000

    Add knexfile with configs

---
+++ recipes/recipe-model.js
@@ -0,0 +1,6 @@
+// //select r.recipe_id, r.title, ri.quantity, ing.name from recipes as r
+// join recipe_ingredients as ri
+// on r.recipe_id = ri.recipe_id
+// join ingredients as ing
+// on ri.ingredient_id = ing.ingredient_id
+// where r.recipe_id=1
